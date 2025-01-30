489ab6979ebf2032203d251bfd6239844a08f673
michael seaman <mseaman26@gmail.com>
2025-01-30 14:27:07 -0800

    trying another

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,30 @@
-const forceDatabaseRefresh = false;
-
 import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
-import path, {dirname} from 'path'; 
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Workaround for __dirname in ES modules using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Serves static files in the entire client's dist folder
-app.use(express.static(path.join(__dirname, '../client/dist')));
+// Serving static files from the client build directory
+app.use(express.static(join(__dirname, '../client/dist')));
 
 app.use(express.json());
 app.use(routes);
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-
+  res.sendFile(join(__dirname, '../client/dist/index.html'));  // Serve the index.html of your React app
 });
 
-sequelize.sync({force: forceDatabaseRefresh}).then(() => {
+sequelize.sync({force: false}).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
